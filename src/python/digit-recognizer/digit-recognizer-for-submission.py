06875c3df06c3ae0c17cfc58f9868f31923681a4
JasonFaas <5844854+JasonFaas@users.noreply.github.com>
2018-10-08 17:24:37 -0700

    Add submission file. Similar to testing file

---
+++ src/python/digit-recognizer/digit-recognizer-for-submission.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2 as cv
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+resources_path = '../../resources/digit-recognizer/'
+# digit_train_set = pd.read_csv('%strain.csv' % resources_path)
+digit_train_set = pd.read_csv('%sjason_train_1000.csv' % resources_path)
+# digit_train_set = pd.read_csv('%sjason_train_10000.csv' % resources_path)
+
+# digit_test_set = pd.read_csv('%stest.csv' % resources_path)
+digit_test_set = pd.read_csv('%sjason_test_1000.csv' % resources_path)
+
+
+independent_columns = digit_train_set.columns[1:]
+dependent_column = digit_train_set.columns[0:1]
+
+x_train = digit_train_set.loc[:, independent_columns].values
+y_train = digit_train_set.loc[:, dependent_column].values
+x_test = digit_test_set.values
+
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(x_train, y_train.ravel())
+pred = knn.predict(x_test)
+result_df = pd.DataFrame(pred)
+result_df.index += 1
+result_df = result_df.reset_index()
+result_df.columns = ['ImageId', 'Label']
+print(result_df)
+result_df.to_csv('%sjason_submission.csv' % resources_path, index=False)
--- src/python/digit-recognizer/digit-recognizer.py
+++ src/python/digit-recognizer/digit-recognizer.py
@@ -6,8 +6,8 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 
 # digit_train_set = pd.read_csv('../../resources/digit-recognizer/train.csv')
-# digit_train_set = pd.read_csv('../../resources/digit-recognizer/jason_1000.csv')
-digit_train_set = pd.read_csv('../../resources/digit-recognizer/jason_10000.csv')
+digit_train_set = pd.read_csv('../../resources/digit-recognizer/jason_1000.csv')
+# digit_train_set = pd.read_csv('../../resources/digit-recognizer/jason_10000.csv')
 
 
 independent_columns = digit_train_set.columns[1:]
@@ -24,12 +24,13 @@ x_train, x_test, y_train, y_test = train_test_split(x,
 scores = {}
 # creating loop for neighbor_itr for later
 # 3 is best, though 1-10 are very similar at about 93.5%
-for neighbor_itr in range(1,10):
+for neighbor_itr in range(3, 4):
     knn = KNeighborsClassifier(n_neighbors=neighbor_itr)
     knn.fit(x_train, y_train.ravel())
     pred = knn.predict(x_test)
     score = accuracy_score(y_test.ravel(), pred)
     print(str(neighbor_itr) + ":" + str(score))
+    print(pred)
 
 
 print("finished")
