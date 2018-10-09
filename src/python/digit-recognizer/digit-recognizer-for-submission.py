ec846ad86a637b8f6365bff5b0823265b5df53cd
JasonFaas <5844854+JasonFaas@users.noreply.github.com>
2018-10-09 10:09:48 -0700

    Add comments.

--- src/python/digit-recognizer/digit-recognizer-for-submission.py
+++ src/python/digit-recognizer/digit-recognizer-for-submission.py
@@ -9,22 +9,26 @@ digit_train_set = pd.read_csv('%sjason_train_1000.csv' % resources_path)
 digit_test_set = pd.read_csv('%stest.csv' % resources_path)
 # digit_test_set = pd.read_csv('%sjason_test_1000.csv' % resources_path)
 
-
+# distinguish columns
 independent_columns = digit_train_set.columns[1:]
 dependent_column = digit_train_set.columns[0:1]
 
+# split train-test data
 x_train = digit_train_set.loc[:, independent_columns].values
 y_train = digit_train_set.loc[:, dependent_column].values
 x_test = digit_test_set.values
 
+# fit training data
 knn = KNeighborsClassifier(n_neighbors=3)
 knn.fit(x_train, y_train.ravel())
-print(x_test.shape)
+
+# setup variables for loop
 yet_to_process = x_test.shape[0]
+result_df = pd.DataFrame(columns=['ImageId', 'Label'])
 processed = 0
 max_to_test = 100
-result_df = pd.DataFrame(columns=['ImageId', 'Label'])
 progress = 0
+
 while yet_to_process > 0:
     # identify how many to process this round
     to_test = min(max_to_test, yet_to_process)
@@ -37,7 +41,7 @@ while yet_to_process > 0:
     temp_df = pd.DataFrame(pred)
     temp_df.index += (1 + processed)
     temp_df = temp_df.reset_index()
-    temp_df.columns = ['ImageId', 'Label']
+    temp_df.columns = result_df.columns
 
     # merge data
     result_df = pd.concat([result_df, temp_df])
@@ -50,7 +54,7 @@ while yet_to_process > 0:
     new_progress = int(100 * processed / x_test.shape[0])
     if new_progress != progress:
         progress = new_progress
-        print(str(processed) + ":" + str(progress))
+        print(str(progress) + "%")
 
 
 result_df.to_csv('%sjason_submission.csv' % resources_path, index=False)
