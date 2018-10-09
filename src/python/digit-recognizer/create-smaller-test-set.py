9545e7feac7e761000817ab9c4240db837ed608d
JasonFaas <5844854+JasonFaas@users.noreply.github.com>
2018-10-08 17:04:22 -0700

    Basic knn implementation for digit-recognizer dataset. Helper file to create smaller test set.

---
+++ src/python/digit-recognizer/create-smaller-test-set.py
@@ -0,0 +1,28 @@
+import numpy as np
+import cv2 as cv
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+resources_path = '../../resources/'
+csv_read = '%sdigit-recognizer/train.csv' % resources_path
+digit_train_set = pd.read_csv(csv_read)
+dts_by_label = {}
+
+total_new_set_size = 1000*10
+row_count = str(total_new_set_size)
+csv_write = '%sdigit-recognizer/jason_%s.csv' % (resources_path, row_count)
+label_count = 10
+
+
+for label_val in range(0, 10):
+    save_count = int(total_new_set_size / label_count)
+    dts_by_label[label_val] = digit_train_set.loc[digit_train_set['label'] == label_val]
+    # print(dts_by_label[label_val].head(save_count))
+
+dts_output = dts_by_label[0].head(save_count)
+for label_val in range(1, 10):
+    dts_output = pd.concat([dts_output, dts_by_label[label_val].head(save_count)])
+
+dts_output.to_csv(csv_write, index=False)
---
+++ src/python/digit-recognizer/digit-recognizer.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2 as cv
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+# digit_train_set = pd.read_csv('../../resources/digit-recognizer/train.csv')
+# digit_train_set = pd.read_csv('../../resources/digit-recognizer/jason_1000.csv')
+digit_train_set = pd.read_csv('../../resources/digit-recognizer/jason_10000.csv')
+
+
+independent_columns = digit_train_set.columns[1:]
+dependent_column = digit_train_set.columns[0:1]
+
+x = digit_train_set.loc[:, independent_columns].values
+y = digit_train_set.loc[:, dependent_column].values
+
+x_train, x_test, y_train, y_test = train_test_split(x,
+                                                    y,
+                                                    test_size=0.2,
+                                                    random_state=91)
+
+scores = {}
+# creating loop for neighbor_itr for later
+# 3 is best, though 1-10 are very similar at about 93.5%
+for neighbor_itr in range(1,10):
+    knn = KNeighborsClassifier(n_neighbors=neighbor_itr)
+    knn.fit(x_train, y_train.ravel())
+    pred = knn.predict(x_test)
+    score = accuracy_score(y_test.ravel(), pred)
+    print(str(neighbor_itr) + ":" + str(score))
+
+
+print("finished")
