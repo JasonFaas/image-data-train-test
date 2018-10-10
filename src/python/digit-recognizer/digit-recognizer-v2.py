7da180dd0504a5a0a847c03f33996c7371a29570
JasonFaas <5844854+JasonFaas@users.noreply.github.com>
2018-10-10 10:26:40 -0700

    Add rotation to v2 for upright numbers

--- src/python/digit-recognizer/digit-recognizer-v2.py
+++ src/python/digit-recognizer/digit-recognizer-v2.py
@@ -1,12 +1,13 @@
 import numpy as np
 import cv2 as cv
+import operator
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from image_test_space import DisplayImage
 
-csv_filename = '../../resources/digit-recognizer/jason_train_1000.csv'
+csv_filename = '../../resources/digit-recognizer/jason_train_2000.csv'
 # csv_filename = '../../resources/digit-recognizer/train.csv'
 digit_train_set = pd.read_csv(csv_filename)
 
@@ -14,103 +15,54 @@ image_info = DisplayImage(csv_filename)
 digit_train_set = image_info.get_all_info()
 
 digit_train_set[:, 1] = list(map(lambda v: cv.dilate(v, np.ones((1,3),dtype=np.uint8)), digit_train_set[:, 1]))
-digit_train_set[:, 1] = list(map(lambda v: cv.threshold(v, 150, 255, cv.THRESH_BINARY)[1], digit_train_set[:, 1]))
-digit_train_set[:, 1] = list(map(lambda v: cv.erode(v, np.ones((1,3),dtype=np.uint8)), digit_train_set[:, 1]))
+digit_train_set[:, 1] = list(map(lambda v: cv.threshold(v, 100, 255, cv.THRESH_TOZERO)[1], digit_train_set[:, 1]))
+digit_train_set[:, 1] = list(map(lambda v: image_info.rotate_to_upright(v), digit_train_set[:, 1]))
 
 x = digit_train_set[:,1]
 y = digit_train_set[:,0]
 y = y.astype('int')
 
 x = list(map(lambda v: np.reshape(v, (-1)), x))
-x_train, x_test, y_train, y_test = train_test_split(x,
-                                                    y,
-                                                    test_size=0.2,
-                                                    random_state=91)
+k_scores = {}
+for rand_state in range(80, 85):
+    print("random_state:" + str(rand_state))
+    x_train, x_test, y_train, y_test = train_test_split(x,
+                                                        y,
+                                                        test_size=0.2,
+                                                        random_state=rand_state)
 
-scores = {}
-# creating loop for neighbor_itr for later
-# 3 is best, though 1-10 are very similar at about 93.5%
-def show_other_info(failed_img):
-    bounded_img = failed_img.copy()
-    _, contours, hierachy = cv.findContours(bounded_img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    cnt = contours[0]
-    x, y, w, h = cv.boundingRect(cnt)
-    cv.rectangle(bounded_img, (x,y), (x+w, y+h), (200), 2)
-    cv.imshow('bounded_rect', bounded_img)
-
-    rotated_rectangle_img = failed_img.copy()
-    rect = cv.minAreaRect(cnt)
-    box = cv.boxPoints(rect)
-    box = np.int0(box)
-    cv.drawContours(rotated_rectangle_img, [box], 0, (200), 2)
-    cv.imshow('rotated', rotated_rectangle_img)
-
-    # rotate box
-    rotate_img = failed_img.copy()
-    min_y_1 = 21
-    min_y_pos_1 = 5
-    min_y_2 = 22
-    min_y_pos_2 = 6
-    for idx, val in enumerate(box):
-        current_y = val[1]
-        if current_y < min_y_1:
-            min_y_2 = min_y_1
-            min_y_1 = current_y
-            min_y_pos_2 = min_y_pos_1
-            min_y_pos_1 = idx
-        elif current_y < min_y_2:
-            min_y_pos_2 = idx
-            min_y_2 = current_y
-    if ((min_y_pos_1 + 1) % 4) == min_y_pos_2:
-        top_left_pos = min_y_pos_1
-    else:
-        top_left_pos = (min_y_pos_1 - 1) % 4
-
-    two_point_distance = lambda x, y: int(((x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2) ** 0.5)
-
-    src_pts = np.array([box[(top_left_pos + 0) % 4],
-                        box[(top_left_pos + 1) % 4],
-                        box[(top_left_pos + 2) % 4],
-                        box[(top_left_pos + 3) % 4]], dtype="float32")
-    top_rect_distance = two_point_distance(box[(top_left_pos + 0) % 4], box[(top_left_pos + 1) % 4])
-    left_rect_distance = two_point_distance(box[(top_left_pos + 1) % 4], box[(top_left_pos + 2) % 4])
-    if top_rect_distance == 0 or left_rect_distance == 0:
-        warped_img = rotate_img
-    else:
-        x_start = int((rotate_img.shape[1] - top_rect_distance) / 2)
-        y_start = int((rotate_img.shape[1] - left_rect_distance) / 2)
-        dst_pts = np.array([
-            [x_start, y_start],
-            [x_start + top_rect_distance, y_start],
-            [x_start + top_rect_distance, y_start + left_rect_distance],
-            [x_start, y_start + left_rect_distance]], dtype="float32")
-        matrix = cv.getPerspectiveTransform(src_pts, dst_pts)
-        warped_img = cv.warpPerspective(rotate_img, matrix, (rotate_img.shape[1], rotate_img.shape[0]))
-
-    cv.imshow('warped', warped_img)
+    # creating loop for neighbor_itr for later
+    # 3 is best, though 1-10 are very similar at about 93.5%
 
 
+    for neighbor_itr in range(3, 4):
+        print("\tk:" + str(neighbor_itr))
+        knn = KNeighborsClassifier(n_neighbors=neighbor_itr)
+        knn.fit(x_train, y_train.ravel())
+        pred = knn.predict(x_test)
+        y_test_raveled = y_test.ravel()
+        score = accuracy_score(y_test_raveled, pred)
+        tf_result = y_test_raveled == pred
+        if not k_scores.get(neighbor_itr):
+            k_scores[neighbor_itr] = {}
+        k_scores[neighbor_itr][rand_state] = score
+        for idx, tf in enumerate(tf_result):
+            if not tf:
+                print("Actual:" + str(y_test_raveled[idx]) + "::Guess:" + str(pred[idx]))
+                failed_img = np.reshape(x_test[idx], (-1, 28, 1)).astype(np.uint8)
+                cv.imshow('img', failed_img)
+                wait_key = cv.waitKey(0) & 0xFF
+                if wait_key == ord('q'):
+                    break
+                elif wait_key == 27:
+                    exit(0)
 
+min_scores = {}
+for i in k_scores.items():
+    min_scores[i[0]] = sorted(i[1].items(), key=operator.itemgetter(1))
+max_min_score = sorted(min_scores.items(), key=operator.itemgetter(1), reverse=True)
+print("best minimum k:" + str(max_min_score[0][0]))
+min_score_of_max = sorted(max_min_score[0][1], key=operator.itemgetter(1))
+print("worst score of k:" + str(min_score_of_max[0][1]))
 
-
-
-
-for neighbor_itr in range(3, 4):
-    knn = KNeighborsClassifier(n_neighbors=neighbor_itr)
-    knn.fit(x_train, y_train.ravel())
-    pred = knn.predict(x_test)
-    y_test_raveled = y_test.ravel()
-    score = accuracy_score(y_test_raveled, pred)
-    tf_result = y_test_raveled == pred
-    print(str(neighbor_itr) + ":" + str(score))
-    for idx, tf in enumerate(tf_result):
-        if not tf:
-            print("Actual:" + str(y_test_raveled[idx]) + "::Guess:" + str(pred[idx]))
-            failed_img = np.reshape(x_test[idx], (-1, 28, 1)).astype(np.uint8)
-            cv.imshow('img', failed_img)
-            show_other_info(failed_img)
-            if cv.waitKey(0) & 0xFF == ord('q'):
-                break
-
-
-print("finished")
+print("\nfinished")
--- src/python/digit-recognizer/image_test_space.py
+++ src/python/digit-recognizer/image_test_space.py
@@ -35,3 +35,65 @@ class DisplayImage:
         get_all_images_from_values = list(map(lambda x: self.get_image_from_values(x[1]), new_array))
         new_array[:,1] = get_all_images_from_values
         return new_array
+
+
+    def rotate_to_upright(self, img):
+        # distance between 2 points lambda
+        two_point_distance = lambda x, y: int(((x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2) ** 0.5)
+
+        # contours
+        _, contours, _ = cv.findContours(img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        if len(contours) == 0:
+            return img
+        cnt = contours[0]
+
+        # vertices of minimum area rectangle
+        rect = cv.minAreaRect(cnt)
+        box = cv.boxPoints(rect)
+        box = np.int0(box)
+
+        # identify top left point
+        min_y_1 = 21
+        min_y_pos_1 = 5
+        min_y_2 = 22
+        min_y_pos_2 = 6
+        for idx, val in enumerate(box):
+            current_y = val[1]
+            if current_y < min_y_1:
+                min_y_2 = min_y_1
+                min_y_1 = current_y
+                min_y_pos_2 = min_y_pos_1
+                min_y_pos_1 = idx
+            elif current_y < min_y_2:
+                min_y_pos_2 = idx
+                min_y_2 = current_y
+        if ((min_y_pos_1 + 1) % 4) == min_y_pos_2:
+            top_left_pos = min_y_pos_1
+        else:
+            top_left_pos = (min_y_pos_1 - 1) % 4
+
+        # calculate source points
+        src_pts = np.array([box[(top_left_pos + 0) % 4],
+                            box[(top_left_pos + 1) % 4],
+                            box[(top_left_pos + 2) % 4],
+                            box[(top_left_pos + 3) % 4]], dtype="float32")
+
+        # calculate destination points
+        top_rect_distance = two_point_distance(box[(top_left_pos + 0) % 4], box[(top_left_pos + 1) % 4])
+        left_rect_distance = two_point_distance(box[(top_left_pos + 1) % 4], box[(top_left_pos + 2) % 4])
+        if top_rect_distance <= 1 or left_rect_distance <= 1:
+            # don't warp if rectangle is too small
+            warped_img = img
+        else:
+            x_start = int((img.shape[1] - top_rect_distance) / 2)
+            y_start = int((img.shape[1] - left_rect_distance) / 2)
+            dst_pts = np.array([
+                [x_start, y_start],
+                [x_start + top_rect_distance, y_start],
+                [x_start + top_rect_distance, y_start + left_rect_distance],
+                [x_start, y_start + left_rect_distance]], dtype="float32")
+
+            # warp image
+            matrix = cv.getPerspectiveTransform(src_pts, dst_pts)
+            warped_img = cv.warpPerspective(img, matrix, (img.shape[1], img.shape[0]))
+        return warped_img
