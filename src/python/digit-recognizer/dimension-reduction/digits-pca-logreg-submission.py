6dbbd503fb52b223d3efb14497ac627eba49338c
JasonFaas <5844854+JasonFaas@users.noreply.github.com>
2018-10-22 11:54:20 -0700

    Basic PCA to LogReg submission

---
+++ src/python/digit-recognizer/dimension-reduction/digits-pca-logreg-submission.py
@@ -0,0 +1,65 @@
+import numpy as np
+import cv2 as cv
+import operator
+import pandas as pd
+from sklearn.decomposition import PCA
+from scipy.stats import pearsonr
+from sklearn.cluster import KMeans
+
+import matplotlib
+matplotlib.use("MacOSX")
+from matplotlib import pyplot as plt
+
+from image_test_space import DisplayImage
+
+resources = '../../../resources/digit-recognizer'
+test_csv = 'test.csv'
+# test_csv = 'jason_test_1000.csv'
+train_csv = 'train.csv'
+# train_csv = 'jason_train_10000.csv'
+# train_csv = 'jason_train_5000.csv'
+# train_csv = 'jason_train_4000.csv'
+# train_csv = 'jason_train_2000.csv'
+# train_csv = 'jason_train_1000.csv'
+train_csv_filename = '%s/%s' % (resources, train_csv)
+test_csv_filename = '%s/%s' % (resources, test_csv)
+
+# read training info
+digit_train_set = pd.read_csv(train_csv_filename)
+image_info = DisplayImage(train_csv_filename)
+digit_train_set = image_info.get_all_info()
+# read testing info
+digit_test_set = pd.read_csv(test_csv_filename)
+x_actual_test = digit_test_set.values
+
+# separate training info into samples and target
+samples_v1 = digit_train_set[:, 1]
+target = digit_train_set[:, 0]
+target = target.astype('int')
+
+samples_v2 = list(map(lambda v: np.reshape(v, (-1)), samples_v1))
+
+from sklearn.preprocessing import StandardScaler
+samples_v2 = StandardScaler().fit_transform(samples_v2)
+
+from sklearn.decomposition import PCA
+model_pca = PCA(0.9)
+samples_v3 = model_pca.fit_transform(samples_v2)
+x_actual_test_tranform = model_pca.transform(x_actual_test)
+
+
+from sklearn.linear_model import LogisticRegression
+logreg = LogisticRegression(solver='lbfgs')
+logreg.fit(samples_v3, target)
+preds = logreg.predict(x_actual_test_tranform)
+# print(logreg.score(x_test, y_test))
+
+
+# put data into dataframe
+result_df = pd.DataFrame(preds)
+result_df.index += (1)
+result_df = result_df.reset_index()
+output_columns = ['ImageId', 'Label']
+result_df.columns = output_columns
+
+result_df.to_csv('temp_submission.csv', index=False)
