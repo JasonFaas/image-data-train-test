e624595f09d3001ff445040d797f2bfd9386319d
JasonFaas <5844854+JasonFaas@users.noreply.github.com>
2018-10-22 09:24:59 -0700

    Add optional image review

--- src/python/digit-recognizer/digit-recognizer-v2.py
+++ src/python/digit-recognizer/digit-recognizer-v2.py
@@ -24,6 +24,9 @@ y = y.astype('int')
 
 x = list(map(lambda v: np.reshape(v, (-1)), x))
 k_scores = {}
+
+review_images = False
+
 for rand_state in range(80, 85):
     print("random_state:" + str(rand_state))
     x_train, x_test, y_train, y_test = train_test_split(x,
@@ -35,7 +38,7 @@ for rand_state in range(80, 85):
     # 3 is best, though 1-10 are very similar at about 93.5%
 
 
-    for neighbor_itr in range(3, 4):
+    for neighbor_itr in range(1, 5):
         print("\tk:" + str(neighbor_itr))
         knn = KNeighborsClassifier(n_neighbors=neighbor_itr)
         knn.fit(x_train, y_train.ravel())
@@ -46,16 +49,17 @@ for rand_state in range(80, 85):
         if not k_scores.get(neighbor_itr):
             k_scores[neighbor_itr] = {}
         k_scores[neighbor_itr][rand_state] = score
-        for idx, tf in enumerate(tf_result):
-            if not tf:
-                print("Actual:" + str(y_test_raveled[idx]) + "::Guess:" + str(pred[idx]))
-                failed_img = np.reshape(x_test[idx], (-1, 28, 1)).astype(np.uint8)
-                cv.imshow('img', failed_img)
-                wait_key = cv.waitKey(0) & 0xFF
-                if wait_key == ord('q'):
-                    break
-                elif wait_key == 27:
-                    exit(0)
+        if review_images:
+            for idx, tf in enumerate(tf_result):
+                if not tf:
+                    print("Actual:" + str(y_test_raveled[idx]) + "::Guess:" + str(pred[idx]))
+                    failed_img = np.reshape(x_test[idx], (-1, 28, 1)).astype(np.uint8)
+                    cv.imshow('img', failed_img)
+                    wait_key = cv.waitKey(0) & 0xFF
+                    if wait_key == ord('q'):
+                        break
+                    elif wait_key == 27:
+                        exit(0)
 
 min_scores = {}
 for i in k_scores.items():
