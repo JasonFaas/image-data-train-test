a59f07cc2d196312080ce889b5d15b9af8a2e55e
JasonFaas <5844854+JasonFaas@users.noreply.github.com>
2018-11-02 12:08:36 -0700

    Add image review

--- src/python/parasite/parasite-cnn-analyze-train-test-different.py
+++ src/python/parasite/parasite-cnn-analyze-train-test-different.py
@@ -8,12 +8,9 @@
 
 import numpy as np
 import cv2 as cv
-import operator
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 from image_test_space import DisplayImage
-from sklearn.linear_model import LogisticRegression
 
 import matplotlib
 from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
@@ -27,7 +24,7 @@ import glob
 
 # Gather images to review
 large_resources = '../../../../image-data-train-test-large-data/Coccidia/img/'
-images_to_review = glob.glob(large_resources + "005*" + ".jpg")
+images_to_review = glob.glob(large_resources + "03*" + ".jpg")
 
 train_images, test_images, _, _ = train_test_split(images_to_review, range(0, len(images_to_review)), test_size=0.2, random_state=91)
 
@@ -46,7 +43,7 @@ x_train_v3 = x_train.astype(np.float32)
 x_train_v3 /= 255
 
 input_shape = (screen_size, screen_size, 3)
-review_failures = False
+review_failures = True
 
 model = Sequential()
 # TODO investigate kernel_size here
@@ -96,6 +93,38 @@ for img_filename in test_images:
             x_img_test_values.append(img_roi)
 
 
+    if review_failures:
+        x_test = np.array(x_img_test_values)
+        x_test_v2 = x_test.astype(np.float32)
+        x_test_v2 /= 255
+
+        predictions = model.predict(np.array(x_test_v2))
+        predictions = list(map(lambda v: np.argmax(np.array(v)), predictions))
+        predictions = np.array(predictions).astype(bool)
+
+        idx = -1
+        img_display = img.copy()
+        for xmin in range(0, img_size, 64):
+            for ymin in range(0, img_size, 64):
+                idx += 1
+                xmax = xmin + screen_size
+                ymax = ymin + screen_size
+                if xmax > img_size:
+                    xmax = img_size
+                    xmin = xmax - screen_size
+                if ymax > img_size:
+                    ymax = img_size
+                    ymin = ymax - screen_size
+                if predictions[idx] and y_img_test_values[idx]:
+                    cv.rectangle(img_display, (xmin, ymin), (xmax, ymax), (0, 255, 0), thickness=2)
+                if predictions[idx] and not y_img_test_values[idx]:
+                    cv.rectangle(img_display, (xmin, ymin), (xmax, ymax), (0, 255, 255), thickness=2)
+                if not predictions[idx] and y_img_test_values[idx]:
+                    cv.rectangle(img_display, (xmin, ymin), (xmax, ymax), (0, 0, 255), thickness=2)
+
+        cv.imshow("img_display", img_display)
+        if cv.waitKey(0) & 0xFF == ord('q'):
+            break
 
 x_test = np.array(x_all_test_values)
 x_test_v2 = x_test.astype(np.float32)
@@ -103,29 +132,16 @@ x_test_v2 /= 255
 
 predictions = model.predict(np.array(x_test_v2))
 predictions = list(map(lambda v: np.argmax(np.array(v)), predictions))
+predictions = np.array(predictions).astype(bool)
 
 y_test = y_all_test_values
 
 # scoring info
 if y_test is not None:
     total_test = len(y_test)
-    predictions = np.array(predictions).astype(bool)
 
     nonzero = np.count_nonzero(np.array(predictions == y_test))
     # print("params:: layer_count: " + str(layer_count) + " " + "layer_size:" + str(layer_size))
     score = round(nonzero / total_test, 3)
     print("\tScore: " + str(score))
     # dataframe.at[layer_count, layer_size] = score
-
-    if review_failures:
-        guess_vs_actual = predictions == y_test
-        for idx, good_guess in enumerate(guess_vs_actual):
-            if not good_guess:
-                print("Guess " + str(predictions[idx]) + " \tActual " + str(y_test[idx]))
-                image = x_test[idx]
-                print(image[screen_size * screen_size:])
-                image = np.reshape(image[0:screen_size * screen_size], (-1, screen_size, 1))
-                image.astype(np.uint8)
-                cv.imshow("failure", image)
-                if cv.waitKey(0) & 0xFF == ord('q'):
-                    break
